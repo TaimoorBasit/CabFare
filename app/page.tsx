ef01cd13ed5ff2eedb50295438652d0ab45a1c00
Taimoor Basit <taimoorbasit@gmail.com>
2026-07-07 03:48:44 -0700

    feat: move customer view from query param to dedicated /viewcustomer route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import CaroleanCoaches from '../components/CaroleanCoaches';
 
-export default function HomePage({ searchParams }: { searchParams?: { [key: string]: string | string[] | undefined } }) {
-  const view = searchParams?.view;
-  const initialMode = view === 'customer' ? 'customer' : 'admin';
-  return <CaroleanCoaches initialMode={initialMode} />;
+export default function HomePage() {
+  return <CaroleanCoaches initialMode="admin" />;
 }
---
+++ app/viewcustomer/page.tsx
@@ -0,0 +1,5 @@
+import CaroleanCoaches from '../../components/CaroleanCoaches';
+
+export default function ViewCustomerPage() {
+  return <CaroleanCoaches initialMode="customer" />;
+}
